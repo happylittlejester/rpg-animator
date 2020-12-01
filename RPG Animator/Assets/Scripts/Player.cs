979a588f0847daee8ee10441bae1ffd557b5c260
NataliaObst <obst.natalia@gmail.com>
2020-12-01 14:12:03 +0100

    Walking & Jump

---
+++ RPG Animator/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
